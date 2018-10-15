e6bd88be8e4ebbba7da6929e42a9e3df19a7eb9a
Quiviro Enquerre <Dev2@GET12-07.local>
2018-10-15 18:33:22 +0200

    descanso

---
+++ src/pages/practica/practica.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the PracticaPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-practica',
+  templateUrl: 'practica.html',
+})
+export class PracticaPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PracticaPage');
+  }
+
+}
