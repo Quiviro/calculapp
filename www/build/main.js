5afff585616c8661521834b719448bda826805c2
Quiviro Enquerre <Dev2@GET12-07.local>
2018-10-17 17:55:10 +0200

    modificada portada Teoría

--- www/build/main.js
+++ www/build/main.js
@@ -4,7 +4,7 @@ webpackJsonp([2],{
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return PracticaPage; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return TeoriaPage; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(27);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -19,29 +19,29 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 
 
 /**
- * Generated class for the PracticaPage page.
+ * Generated class for the TeoriaPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-var PracticaPage = (function () {
-    function PracticaPage(navCtrl, navParams) {
+var TeoriaPage = (function () {
+    function TeoriaPage(navCtrl, navParams) {
         this.navCtrl = navCtrl;
         this.navParams = navParams;
     }
-    PracticaPage.prototype.ionViewDidLoad = function () {
-        console.log('ionViewDidLoad PracticaPage');
+    TeoriaPage.prototype.ionViewDidLoad = function () {
+        console.log('ionViewDidLoad TeoriaPage');
     };
-    PracticaPage = __decorate([
+    TeoriaPage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-practica',template:/*ion-inline-start:"/Users/Dev2/Documents/201810-JavaScript_Ionic-CFTIC/Ionic/calculapp/src/pages/practica/practica.html"*/`<ion-header>\n  <ion-navbar>\n    <button ion-button menuToggle>\n      <ion-icon name="finger-print"></ion-icon>\n    </button>\n    <ion-title>Práctica</ion-title>\n  </ion-navbar>\n</ion-header>\n\n<ion-content padding>\n  <h1 text-center>Elige tu configuración</h1>\n\n  <p>\n    \n  </p>\n\n  <button ion-button secondary menuToggle text-centered>\n    <ion-icon left name="finger-print"> Menu</ion-icon>\n  </button>\n</ion-content>\n`/*ion-inline-end:"/Users/Dev2/Documents/201810-JavaScript_Ionic-CFTIC/Ionic/calculapp/src/pages/practica/practica.html"*/,
+            selector: 'page-teoria',template:/*ion-inline-start:"/Users/Dev2/Documents/201810-JavaScript_Ionic-CFTIC/Ionic/calculapp/src/pages/teoria/teoria.html"*/`<ion-header>\n    <ion-navbar>\n      <button ion-button menuToggle>\n        <ion-icon name="finger-print"></ion-icon>\n      </button>\n      <ion-title>Teoría</ion-title>\n    </ion-navbar>\n  </ion-header>\n  \n  <ion-content padding>\n    <h2 text-center>¿Necesitas ayuda?</h2>\n\n    <ion-segment [(ngModel)]="mymodel" class="botonera">\n      <ion-segment-button value="teoriaSumar">\n        <ion-icon name="add"></ion-icon>\n        <br>\n        <span>Sumar</span>\n      </ion-segment-button>\n      <ion-segment-button value="teoriaRestar">\n        <ion-icon name="remove"></ion-icon>\n        <br>\n        <span>Restar</span>\n      </ion-segment-button>\n      <ion-segment-button value="teoriaMultiplicar">\n        <ion-icon name="close"></ion-icon>\n        <br>\n        <span>Multiplicar</span>\n      </ion-segment-button>\n      <ion-segment-button value="teoriaDividir">\n        <ion-icon name="cut"></ion-icon>\n        <br>\n        <span>Dividir</span>\n      </ion-segment-button>\n    </ion-segment>\n    <div [ngSwitch]="mymodel" text-wrap>\n      <ion-list *ngSwitchCase="">\n        <ion-item>\n          <p>Pulsa en la operación de la que quieres repasar la teoría para ver una pequeña explicación.</p>\n        </ion-item>\n      </ion-list>\n      <ion-list *ngSwitchCase="\'teoriaSumar\'">\n        \n        <ion-item>\n          <h2 text-center>SUMAR</h2>\n          <p>Sumar es juntar 2 o más cosas para saber cuántas hay en total.</p>\n          <br>\n          <p>Por ejemplo: Pedro tiene 3 balones y María tiene 6. Si juntamos a Pedro y a María, ¿cuántos balones tienen en total? 3 + 6 = 9</p>\n          <br>\n          <p>Cuéntalos y verás que hay 9 balones:</p>\n          <img src="../../assets/imgs/suma1.png">\n          <br>\n          <br>\n          <p>Ahora vamos a aprender a sumar con los dedos: En una mano ponemos 2 dedos y en la otra 3 dedos. Y ahora contamos todos los dedos que tenemos: 2 dedos + 3 dedos, es igual a 5 dedos.</p>\n          <img src="../../assets/imgs/suma2.png">\n          <br>\n          <p>¿Preparado para comenzar?</p>\n        </ion-item>\n      </ion-list>\n      <ion-list *ngSwitchCase="\'teoriaRestar\'">\n        <ion-item>Lorem ipsum dolor... RESTAR</ion-item>\n      </ion-list>\n      <ion-list *ngSwitchCase="\'teoriaMultiplicar\'">\n        <ion-item>Lorem ipsum... MULTIPLICAR</ion-item>\n      </ion-list>\n      <ion-list *ngSwitchCase="\'teoriaDividir\'">\n        <ion-item>Lorem ipsum dolor... DIVIDIR</ion-item>\n      </ion-list>\n    </div>\n\n    <button ion-button color="secondary" menuToggle text-centered>\n      <ion-icon left name="finger-print"> Menu</ion-icon>\n    </button>\n  </ion-content>\n  <ion-tabs>\n      <ion-tab [root]="tab1Root" tabTitle="Sumar" tabIcon="add"></ion-tab>\n      <ion-tab [root]="tab2Root" tabTitle="Restar" tabIcon="remove"></ion-tab>\n      <ion-tab [root]="tab3Root" tabTitle="Multiplicar" tabIcon="close"></ion-tab>\n      <ion-tab [root]="mienlace" tabTitle="Dividir" tabIcon="barcode"></ion-tab>\n    </ion-tabs>\n`/*ion-inline-end:"/Users/Dev2/Documents/201810-JavaScript_Ionic-CFTIC/Ionic/calculapp/src/pages/teoria/teoria.html"*/,
         }),
         __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* NavParams */]])
-    ], PracticaPage);
-    return PracticaPage;
+    ], TeoriaPage);
+    return TeoriaPage;
 }());
 
-//# sourceMappingURL=practica.js.map
+//# sourceMappingURL=teoria.js.map
 
 /***/ }),
 
@@ -67,11 +67,11 @@ webpackEmptyAsyncContext.id = 111;
 
 var map = {
 	"../pages/practica/practica.module": [
-		275,
+		274,
 		1
 	],
 	"../pages/teoria/teoria.module": [
-		274,
+		275,
 		0
 	]
 };
@@ -208,8 +208,8 @@ Object(__WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__["a" /* pl
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__app_component__ = __webpack_require__(265);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__pages_home_home__ = __webpack_require__(196);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__pages_list_list__ = __webpack_require__(197);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__pages_practica_practica__ = __webpack_require__(100);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__pages_teoria_teoria__ = __webpack_require__(99);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__pages_practica_practica__ = __webpack_require__(99);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__pages_teoria_teoria__ = __webpack_require__(100);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__ionic_native_status_bar__ = __webpack_require__(192);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__ionic_native_splash_screen__ = __webpack_require__(195);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -244,8 +244,8 @@ var AppModule = (function () {
                 __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
                 __WEBPACK_IMPORTED_MODULE_2_ionic_angular__["c" /* IonicModule */].forRoot(__WEBPACK_IMPORTED_MODULE_3__app_component__["a" /* MyApp */], {}, {
                     links: [
-                        { loadChildren: '../pages/teoria/teoria.module#TeoriaPageModule', name: 'TeoriaPage', segment: 'teoria', priority: 'low', defaultHistory: [] },
-                        { loadChildren: '../pages/practica/practica.module#PracticaPageModule', name: 'PracticaPage', segment: 'practica', priority: 'low', defaultHistory: [] }
+                        { loadChildren: '../pages/practica/practica.module#PracticaPageModule', name: 'PracticaPage', segment: 'practica', priority: 'low', defaultHistory: [] },
+                        { loadChildren: '../pages/teoria/teoria.module#TeoriaPageModule', name: 'TeoriaPage', segment: 'teoria', priority: 'low', defaultHistory: [] }
                     ]
                 }),
             ],
@@ -281,8 +281,8 @@ var AppModule = (function () {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__ionic_native_status_bar__ = __webpack_require__(192);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__ = __webpack_require__(195);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__pages_home_home__ = __webpack_require__(196);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__pages_practica_practica__ = __webpack_require__(100);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__pages_teoria_teoria__ = __webpack_require__(99);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__pages_practica_practica__ = __webpack_require__(99);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__pages_teoria_teoria__ = __webpack_require__(100);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__pages_list_list__ = __webpack_require__(197);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -350,7 +350,7 @@ var MyApp = (function () {
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return TeoriaPage; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return PracticaPage; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(27);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -365,29 +365,29 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 
 
 /**
- * Generated class for the TeoriaPage page.
+ * Generated class for the PracticaPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-var TeoriaPage = (function () {
-    function TeoriaPage(navCtrl, navParams) {
+var PracticaPage = (function () {
+    function PracticaPage(navCtrl, navParams) {
         this.navCtrl = navCtrl;
         this.navParams = navParams;
     }
-    TeoriaPage.prototype.ionViewDidLoad = function () {
-        console.log('ionViewDidLoad TeoriaPage');
+    PracticaPage.prototype.ionViewDidLoad = function () {
+        console.log('ionViewDidLoad PracticaPage');
     };
-    TeoriaPage = __decorate([
+    PracticaPage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-teoria',template:/*ion-inline-start:"/Users/Dev2/Documents/201810-JavaScript_Ionic-CFTIC/Ionic/calculapp/src/pages/teoria/teoria.html"*/`<ion-header>\n    <ion-navbar>\n      <button ion-button menuToggle>\n        <ion-icon name="finger-print"></ion-icon>\n      </button>\n      <ion-title>Teoría</ion-title>\n    </ion-navbar>\n  </ion-header>\n  \n  <ion-content padding>\n    <h2 text-center>Vamos a repasar un poco antes de calcular</h2>\n\n    <ion-segment [(ngModel)]="mymodel" class="botonera">\n      <ion-segment-button value="teoriaSumar">\n        <ion-icon name="add"></ion-icon>\n        <br>\n        <span>Sumar</span>\n      </ion-segment-button>\n      <ion-segment-button value="teoriaRestar">\n        <ion-icon name="remove"></ion-icon>\n        <br>\n        <span>Restar</span>\n      </ion-segment-button>\n      <ion-segment-button value="teoriaMultiplicar">\n        <ion-icon name="close"></ion-icon>\n        <br>\n        <span>Multiplicar</span>\n      </ion-segment-button>\n      <ion-segment-button value="teoriaDividir">\n        <ion-icon name="cut"></ion-icon>\n        <br>\n        <span>Dividir</span>\n      </ion-segment-button>\n    </ion-segment>\n    <div [ngSwitch]="mymodel" text-wrap>\n      <ion-list *ngSwitchCase="\'teoriaSumar\'">\n        <ion-item>\n          <h3 text-center>SUMAR</h3>\n          <p>Sumar es juntar 2 o más cosas para saber cuántas hay en total.</p>\n          <br>\n          <p>Por ejemplo: Pedro tiene 3 balones y María tiene 6. Si juntamos a Pedro y a María, ¿cuántos balones tienen en total? 3 + 6 = 9</p>\n          <br>\n          <p>Cuéntalos y verás que hay 9 balones:</p>\n          <img src="../../assets/imgs/suma1.png">\n          <br>\n          <p>Ahora vamos a aprender a sumar con los dedos: En una mano ponemos 2 dedos y en la otra 3 dedos. Y ahora contamos todos los dedos que tenemos: 2 dedos + 3 dedos, es igual a 5 dedos.</p>\n          <img src="../../assets/imgs/suma2.png">\n          <br>\n          <p>¿Preparado para comenzar?</p>\n        </ion-item>\n      </ion-list>\n      <ion-list *ngSwitchCase="\'teoriaRestar\'">\n        <ion-item>Lorem ipsum dolor... RESTAR</ion-item>\n      </ion-list>\n      <ion-list *ngSwitchCase="\'teoriaMultiplicar\'">\n        <ion-item>Lorem ipsum... MULTIPLICAR</ion-item>\n      </ion-list>\n      <ion-list *ngSwitchCase="\'teoriaDividir\'">\n        <ion-item>Lorem ipsum dolor... DIVIDIR</ion-item>\n      </ion-list>\n    </div>\n\n    <button ion-button color="secondary" menuToggle text-centered>\n      <ion-icon left name="finger-print"> Menu</ion-icon>\n    </button>\n  </ion-content>\n  <ion-tabs>\n      <ion-tab [root]="tab1Root" tabTitle="Sumar" tabIcon="add"></ion-tab>\n      <ion-tab [root]="tab2Root" tabTitle="Restar" tabIcon="remove"></ion-tab>\n      <ion-tab [root]="tab3Root" tabTitle="Multiplicar" tabIcon="close"></ion-tab>\n      <ion-tab [root]="mienlace" tabTitle="Dividir" tabIcon="barcode"></ion-tab>\n    </ion-tabs>\n`/*ion-inline-end:"/Users/Dev2/Documents/201810-JavaScript_Ionic-CFTIC/Ionic/calculapp/src/pages/teoria/teoria.html"*/,
+            selector: 'page-practica',template:/*ion-inline-start:"/Users/Dev2/Documents/201810-JavaScript_Ionic-CFTIC/Ionic/calculapp/src/pages/practica/practica.html"*/`<ion-header>\n  <ion-navbar>\n    <button ion-button menuToggle>\n      <ion-icon name="finger-print"></ion-icon>\n    </button>\n    <ion-title>Práctica</ion-title>\n  </ion-navbar>\n</ion-header>\n\n<ion-content padding>\n  <h1 text-center>Elige tu configuración</h1>\n\n  <p>\n    \n  </p>\n\n  <button ion-button secondary menuToggle text-centered>\n    <ion-icon left name="finger-print"> Menu</ion-icon>\n  </button>\n</ion-content>\n`/*ion-inline-end:"/Users/Dev2/Documents/201810-JavaScript_Ionic-CFTIC/Ionic/calculapp/src/pages/practica/practica.html"*/,
         }),
         __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* NavParams */]])
-    ], TeoriaPage);
-    return TeoriaPage;
+    ], PracticaPage);
+    return PracticaPage;
 }());
 
-//# sourceMappingURL=teoria.js.map
+//# sourceMappingURL=practica.js.map
 
 /***/ })
 
